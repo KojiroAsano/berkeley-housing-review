7334c373a21e72486986e314b4a256b1d7b5885d
Kojiro Asano <45510175+KojiroAsano@users.noreply.github.com>
2023-05-01 14:13:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ function update(email, amount) {
   });
 }
 
-var port = process.env.PORT || 3000;
+var port = 17893 || process.env.PORT;
 app.listen(port);
 console.log("Running on port: " + port);
