42051b81ae9fb8a5f8cc87378c9f57b6258e1d3c
Kojiro Asano <45510175+KojiroAsano@users.noreply.github.com>
2023-05-02 15:07:07 -0700

    Update blackwell.js

--- public/blackwell.js
+++ public/blackwell.js
@@ -9,7 +9,7 @@ const Blackwell = () => {
             <b>Address:</b> 2401 Durant Ave, Berkeley, CA 94720
           </h4>
           <p>
-            <b>Description:</b> most Annoying Place in the World!!
+            <b>Description:</b> UC Berkeley’s newest housing complex, with premium-style double rooms in a lively location just one block from the south side of campus. Close to shops, restaurants, the Recreational Sports Facility and Sproul Plaza.
           </p>
         </div>
 
